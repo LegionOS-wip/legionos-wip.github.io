429389ee1d798cc2694920be294a39889bd84523
Rajkale99 <kaleraj.rk@gmail.com>
2023-05-25 16:17:21 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Changelog from './views/Changelog.vue';
 
 export const routes = [
   { path: '', component: Home, name: 'home' },
-  { path: '/changelog', component: Changelog, name: 'changelog' },
+  { path: '/download/changelog', component: Changelog, name: 'changelog' },
   {
     path: '/:codename',
     component: Device,
